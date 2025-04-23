3a8e754307ceb12e3a32c5e308b6f675f40d57cd
PankajKumar23z <pankajkumar357@gmail.com>
2025-04-23 21:20:21 +0530

    2 files added

---
+++ src/test/java/com/thetestingsite/ex_15092024/CRUD/GET/NonBDDStyle.java
@@ -0,0 +1,30 @@
+package com.thetestingsite.ex_15092024.CRUD.GET;
+
+import io.restassured.RestAssured;
+import io.restassured.specification.RequestSpecification;
+
+public class NonBDDStyle {
+
+    static RequestSpecification r =  RestAssured.given();
+
+    public static void main(String[] args) {
+
+        r.baseUri("https://api.zippopotam.us");
+        testnon_bdd_1();
+        testnon_bdd_2();
+    }
+
+    private static void testnon_bdd_1() {
+
+        r.basePath("/IN/560037");
+        r.when().get();
+        r.then().log().all().statusCode(404);
+    }
+
+    private static void testnon_bdd_2() {
+
+        r.basePath("/IN/-1");
+        r.when().get();
+        r.then().log().all().statusCode(404);
+    }
+}
